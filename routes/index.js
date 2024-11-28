28ed3e07401d636edace406b55c71de71a509525
Kenstler0 <semirasan76@gmail.com>
2024-11-28 11:58:16 +0300

    пункт 4.2

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,15 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+router.get('/flycatcher', function(req, res, next) {
+  res.send("<h1>Страница Мухоловки</h1>")
+});
 
+router.get('/goldenrod', function(req, res, next) {
+  res.send("<h1>Страница Золотарника</h1>")
+});
+
+router.get('/bamboo', function(req, res, next) {
+  res.send("<h1>Страница Бамбука</h1>")
+});
 module.exports = router;
