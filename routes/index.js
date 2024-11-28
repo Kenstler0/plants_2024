8a6c5095e9da8d44d043881c3a9d02a417bae877
Kenstler0 <semirasan76@gmail.com>
2024-11-28 12:29:47 +0300

    plants_4.3

--- routes/index.js
+++ routes/index.js
@@ -6,14 +6,24 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 router.get('/flycatcher', function(req, res, next) {
-  res.send("<h1>Страница Мухоловки</h1>")
+  res.render('plant', {
+  title: "Венерина Мухоловка",
+  picture: "images/flycatcher.jpg",
+  desc:"Вене́рина мухоло́вка — вид хищных растений из монотипного рода Дионея семейства Росянковые"
+  });
 });
-
-router.get('/goldenrod', function(req, res, next) {
-  res.send("<h1>Страница Золотарника</h1>")
-});
-
 router.get('/bamboo', function(req, res, next) {
-  res.send("<h1>Страница Бамбука</h1>")
+  res.render('plant', {
+  title: "Бамбук",
+  picture: "images/bamboo.jpg",
+  desc:"Бамбу́к — род многолетних вечнозелёных растений семейства Злаки, из подсемейства Бамбуковые."
+  });
+});
+router.get('/goldenrod', function(req, res, next) {
+  res.render('plant', {
+  title: "Золотарник",
+  picture: "images/goldenrod.jpg",
+  desc:"Золота́рник обыкнове́нный — многолетнее травянистое растение; вид рода Золотарник семейства Астровые."
+  });
 });
 module.exports = router;
